be431425baa541e3fbc48a47da7d0742e2a69c06
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-02-15 06:21:05 -0300

    Commit Update

---
+++ _teste0/01. Pre-Classic/03. rd-132211/_teste1/08.ab [fail]/src/phys/AABB.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Mathematics;
+
+namespace RubyDung.src.phys;
+
+public class AABB {
+    public Vector3 Min {
+        get; set;
+    }
+
+    public Vector3 Max {
+        get; set;
+    }
+
+    public AABB(Vector3 min, Vector3 max) {
+        Min = min;
+        Max = max;
+    }
+
+    public bool Intersects(AABB other) {
+        return (Min.X <= other.Max.X && Max.X >= other.Min.X) &&
+               (Min.Y <= other.Max.Y && Max.Y >= other.Min.Y) &&
+               (Min.Z <= other.Max.Z && Max.Z >= other.Min.Z);
+
+    }
+}
